ec1647f794ca71b5797b1a1476c03ee6067db2fb
Abhishek B V <VAbhishek@ESTL-AbhishekG.estuate.com>
2018-02-07 08:41:34 +0530

    test

--- playground/test/test.js
+++ playground/test/test.js
@@ -3,17 +3,17 @@ const Mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 4001;
 // Mongoose.connect('mongodb://localhost:27017/newTodo');
-Mongoose.connect('mongodb://AbhishekVG:opensimsim@M13@ds125578.mlab.com:25578/todoapp');
-const newtodo = Mongoose.model('newTodo', {
-    text: {
-        type: String,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
+// Mongoose.connect('mongodb://AbhishekVG:opensimsim@M13@ds125578.mlab.com:25578/todoapp');
+// const newtodo = Mongoose.model('newTodo', {
+//     text: {
+//         type: String,
+//         required: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// });
 
 // Todo.save().then((data) => {
 // console.log("DATA = ", data)
@@ -26,7 +26,8 @@ app.get('/',(req, res) => {
 });
 
 app.get('/:id',(req, res) => {
-    const Todo = new newtodo({text: 'first app'});
+    // const Todo = new newtodo({text: 'first app'});
+    res.send({"success": '1234'})
     // Todo.save().then((data) => {
     //     console.log("DATA = ", data)
     //     }).catch((err) => {
@@ -38,12 +39,12 @@ app.get('/:id',(req, res) => {
     //     console.log("Finding success = ",docs)
 
     //   });
-    newtodo.findById(req.params.id).then((data) => {
-        console.log("Finding success = ",data, req.params)
-        res.status(301).send(data);
-    }).catch((err) => {
-        console.log("err", err);
-    })
+    // newtodo.findById(req.params.id).then((data) => {
+    //     console.log("Finding success = ",data, req.params)
+    //     res.status(301).send(data);
+    // }).catch((err) => {
+    //     console.log("err", err);
+    // })
 });
 
 app.listen(port, (err, x) => {
